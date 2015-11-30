6b8c1620346c7a77d8207e17cd8a42496661db1e
Dianne Dietrich <dd388@cornell.edu>
2015-11-30 12:01:14 -0500

    Update dvd2dv25.py

--- dvd2dv25.py
+++ dvd2dv25.py
@@ -1,8 +1,7 @@
-#Created at AMIA Hackday2015 by Dianne Dietrich, Morgan Morel, Brendan Coates, Tre Berney, and Sadie Roosa
+#!/usr/bin/env python
 
-#!/usr/bin/python
+# Created at AMIA Hackday2015 by Dianne Dietrich, Morgan Morel, Brendan Coates, Tre Berney, and Sadie Roosa
 
-# import modules used here -- sys is a very standard one
 import sys
 import glob
 import os
@@ -14,16 +13,14 @@ import time
 
 TIMEDATE = time.strftime("%Y%m%d%H%M%S")
 
-# Gather our code in a main() function
 def main():
-
-    # Stuff # vars get called PathTo here
     PathToOutput = SetupOutput(sys.argv[2])
     PathToVTS = TestISO(sys.argv[1])
     DictOfVTS = VOBDict(PathToVTS)
     ThisVOBLists = CreateVOBLists(DictOfVTS, PathToOutput)
     ffmpegConcat(ThisVOBLists, PathToOutput)
-    AllConcats = glob.glob('{0}/concats/*.VOB'.format(PathToOutput))
+    AllConcatsPath = os.path.join(PathToOutput, 'concats', '*.VOB')
+    AllConcats = glob.glob(AllConcatsPath)
     for ac in AllConcats:
         thisHeight, thisDAR = ffprobe(ac)
         createFinal(PathToOutput, ac, thisHeight, thisDAR)
@@ -32,8 +29,8 @@ def main():
     
 def deleteWorkFiles(OutputPath):
     #Deletes /voblist and /concats in the output folder
-    rmtree('{0}/voblists'.format(OutputPath,))
-    rmtree('{0}/concats'.format(OutputPath,))
+    rmtree(os.path.join(OutputPath, 'voblists'))
+    rmtree(os.path.join(OutputPath, 'concats'))
 
 def SetupOutput(OutputPath):
     OutputPath = os.path.expanduser(OutputPath)
@@ -41,9 +38,9 @@ def SetupOutput(OutputPath):
         sys.exit('Output directory does not exist.')
     else:
         try:
-            os.makedirs('{0}/voblists'.format(OutputPath,))
-            os.makedirs('{0}/concats'.format(OutputPath,))
-            os.makedirs('{0}/final_{1}'.format(OutputPath,TIMEDATE,))
+            os.makedirs(os.path.join(OutputPath, 'voblists'))
+            os.makedirs(os.path.join(OutputPath, 'concats'))
+            os.makedirs(os.path.join(OutputPath, 'final_{0}'.format(TIMEDATE)))
         except:
             sys.exit('Please make sure the output directory does not contain /voblist or /concats !!')
     return OutputPath # Returns validated output path
@@ -63,7 +60,7 @@ def VOBDict(VTSPath):
     vtsdict = defaultdict(list)
     for i in range(1,100):
         vtsindex = str(i).zfill(2)
-        vtsbase = '{0}/VTS_{1}'.format(VTSPath,vtsindex)
+        vtsbase = os.path.join(VTSPath, 'VTS_{0}'.format(vtsindex))
         for res in glob.glob('{0}*.VOB'.format(vtsbase)):
             vtsdict[vtsbase].append(res)
     return vtsdict
@@ -71,18 +68,19 @@ def VOBDict(VTSPath):
 
 def CreateVOBLists(VTSDictionary, OutputPath):
     for f in VTSDictionary.iterkeys():
-        textlist = '{0}/voblists/{1}_list.txt'.format(OutputPath, os.path.basename(f))
+        textlist = os.path.join(OutputPath, 'voblists', '{0}_list.txt'.format(os.path.basename(f)))
+        VTSDictionary[f].sort()
         with open(textlist, 'wb') as textlistfile:
             for vobfile in VTSDictionary[f]:
                 textlistfile.write("file '{0}'\n".format(vobfile))
-    return glob.glob('{0}/voblists/*.txt'.format(OutputPath))
+    return glob.glob(os.path.join(OutputPath, 'voblists', '*.txt'))
 
 
 def ffmpegConcat(VOBLists, OutputPath):
     for vl in VOBLists:
         vlbasename = os.path.basename(vl.split('_list.txt')[0])
         vlbasename = '{0}_all.VOB'.format(vlbasename)
-        vlbasename = '{0}/concats/{1}'.format(OutputPath, vlbasename)
+        vlbasename = os.path.join(OutputPath, 'concats', vlbasename)
         subprocess.check_output(['ffmpeg', '-f', 'concat', '-i', vl, '-c', 'copy', vlbasename])
 
 
@@ -90,23 +88,26 @@ def ffprobe(InputVOB):
     res = subprocess.check_output(['ffprobe', '-show_streams', '-i', InputVOB])
     for line in res.split('\n'):
         if line.startswith('height'):
-            vobHeight = line.split('=', 1)[1]
+            vobHeight = line.split('=', 1)[1].strip()
         if line.startswith('display_aspect_ratio'):
-            vobDAR = line.split('=', 1)[1]
+            vobDAR = line.split('=', 1)[1].strip()
     return vobHeight, vobDAR
 
 def createFinal(OutputPath, InputFile, InputHeight, InputDAR):
     FinalVOB = os.path.basename(InputFile).split('_all.VOB')[0]
-    FinalVOB = '{0}/final_{1}/{2}_final.dv'.format(OutputPath, TIMEDATE, FinalVOB,)
+    FinalVOB = os.path.join(OutputPath, 'final_{0}'.format(TIMEDATE), '{0}_final.dv'.format(FinalVOB))
     if InputHeight == '480':
         FinalFormat = 'ntsc-dv'
     elif InputHeight == '576':
         FinalFormat = 'pal-dv'
     else:
         FinalFormat = 'ntsc-dv'
-    subprocess.check_output(['ffmpeg', '-i', InputFile, '-target', FinalFormat, '-aspect', InputDAR, FinalVOB])
+    try:
+        subprocess.check_output(['ffmpeg', '-i', InputFile, '-target', FinalFormat, '-aspect', InputDAR, FinalVOB])
+    except subprocess.CalledProcessError as e:
+        print e.output
+        sys.exit(1)
 
-# Standard boilerplate to call the main() function to begin
-# the program.
+
 if __name__ == '__main__':
   main()
